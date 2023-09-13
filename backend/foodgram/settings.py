eda9e7d946fb99aeba78911d24ca5a0ada42514a
Олег Жугрин <o43zhu@gmail.com>
2023-09-13 14:41:30 +0300

    last fixes

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -115,11 +115,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static/')
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/app/media/'
 
 DJOSER = {
     'SERIALIZERS': {
